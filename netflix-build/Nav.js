967ebb5c0982caf7d1828ce52c1768360c51f4e8
el <elidakirigo@gmail.com>
2023-04-20 21:53:19 +0300

    `

---
+++ netflix-build/Nav.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './Nav.css';
+
+export function Nav(props) {
+	
+
+	return (
+	<div className='nav'></div>
+	)
+}
---
+++ netflix-build/src/HomeScreen.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './HomeScreen.css';
+
+export function HomeScreen() {
+	
+
+	return (
+		<div className='homescreen'></div>
+	)
+}
