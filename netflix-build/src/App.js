e5de590f81e25810993142c8856097b63d00cb1b
el <elidakirigo@gmail.com>
2023-04-20 22:54:51 +0300

    `

--- netflix-build/src/App.js
+++ netflix-build/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import { HomeScreen } from './HomeScreen';
 
 function App() {
   return (
--- netflix-build/src/HomeScreen.js
+++ netflix-build/src/HomeScreen.js
@@ -1,10 +1,15 @@
 import React from 'react'
 import './HomeScreen.css';
+import {Nav} from './Nav';
 
-export function HomeScreen() {
-	
-
+export function HomeScreen() { 
 	return (
-		<div className='homescreen'></div>
+		<div className='homescreen'>
+			{/* nav */}
+			<Nav/>
+			{/* banner */}
+
+			{/* row */}
+		</div>
 	)
 }
