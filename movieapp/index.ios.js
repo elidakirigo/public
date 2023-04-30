06425cacb678f3ee9030ec15003d655b466a271f
Elida (moon pie) <elidakirigo@gmail.com>
2023-04-30 21:58:45 +0300

    Delete movieapp directory

--- movieapp/index.ios.js
+++
@@ -1,4 +0,0 @@
-/* eslint-disable no-unused-vars */
-import App from './src/app';
-
-const app = new App();
--- movieapp/src/reducers/rootReducer.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux';
-import movies from '../modules/movies/movies.reducer';
-
-const rootReducer = combineReducers({
-	movies
-});
-
-export default rootReducer;
