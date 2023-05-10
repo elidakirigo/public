1d2f1d48f5532514ad372bf62704648bb5ba98ad
el <elidakirigo@gmail.com>
2023-05-10 15:01:03 +0300

    `

--- FEM/article-preview-component-master/JS/script.js
+++
@@ -1,5 +0,0 @@
-function showIcons() {
-    let container = document.querySelector('.container');
-    container.style.display = 'flex';
-    setTimeout(() => { container.style.display = 'none'}, 8000)
-}
--- FEM/coding-bootcamp-testimonials-slider-master/JS/script.js
+++
@@ -1,8 +0,0 @@
-const testimonial1 = document.querySelector(".first-testimonial")
-const testimonial2 = document.querySelector(".sec-testimonial")
-
-const buttonClick = () => {
-    console.log('clicked');
-      testimonial1.classList.toggle("none");
-      testimonial2.classList.toggle("none");
-}
