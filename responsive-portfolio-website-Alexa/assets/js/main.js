bd2f2bca9d71802f634de16949e9c3d66c50746f
elida <elidakirigo@gmail.com>
2023-08-15 19:00:42 +0300

    feat:what i do

--- responsive-portfolio-website-Alexa/assets/js/main.js
+++ responsive-portfolio-website-Alexa/assets/js/main.js
@@ -50,9 +50,46 @@ skillsHeader.forEach((el) => {
 	el.addEventListener('click', toggleSkills)
 })
 /*==================== QUALIFICATION TABS ====================*/
+const tabs = document.querySelectorAll('[data-target'),
+	tabContents = document.querySelectorAll('[data-content')
 
+tabs.forEach((tab) => {
+	tab.addEventListener('click', () => {
+		const target = document.querySelector(tab.dataset.target)
+
+		tabContents.forEach((tabContent) => {
+			tabContent.classList.remove('qualification__active')
+		})
+		target.classList.add('qualification__active')
+
+		tabs.forEach(tab=>{
+			tab.classList.remove('qualification__active')
+		})
+		tab.classList.add('qualification__active')
+	})
+})
 /*==================== SERVICES MODAL ====================*/
+const modalViews = document.querySelectorAll('.services__modal'),
+modalBtns = document.querySelectorAll('.services__button'),
+modalCloses = document.querySelectorAll('.services__modal-close')
 
+let modal =function(modalClick){
+	modalViews[modalClick].classList.add('active-modal')
+}
+
+modalBtns.forEach((modalBtn,i)=>{
+	modalBtn.addEventListener('click',()=>{
+		modal(i)
+	})
+})
+
+modalCloses.forEach((modalClose)=>{
+	modalClose.addEventListener('click',()=>{
+		modalViews.forEach(modalView=>{
+			modalView.classList.remove('active-modal')
+		})
+	})
+})
 /*==================== PORTFOLIO SWIPER  ====================*/
 
 /*==================== TESTIMONIAL ====================*/
