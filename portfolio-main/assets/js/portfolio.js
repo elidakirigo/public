dccbaa320e0ee2d9713284e4885fbba91ef4c009
elida <elidakirigo@gmail.com>
2023-10-15 02:46:27 +0300

    `

--- portfolio-main/assets/js/portfolio.js
+++ portfolio-main/assets/js/portfolio.js
@@ -5,7 +5,7 @@ const allPosts = [
 	// API AUthetication
 	{
 		topic: 'feature',
-		link: 'https://github.com/Chrissiku/rails-api-authentication-crud',
+		link: 'https://github.com/elidakirigo/rails-api-authentication-crud',
 		title: 'Hotel Booking',
 		content: ``,
 		stacks: [
@@ -17,14 +17,14 @@ const allPosts = [
 				name: 'Bootstrap 5',
 			},
 		],
-		live: [true, 'https://github.com/Chrissiku/rails-api-authentication-crud'],
-		source: [true, 'https://github.com/Chrissiku/rails-api-authentication-crud'],
+		live: [true, 'https://github.com/elidakirigo/rails-api-authentication-crud'],
+		source: [true, 'https://github.com/elidakirigo/rails-api-authentication-crud'],
 		image: 'https://user-images.githubusercontent.com/101924220/209710207-edf76c66-5034-4cf0-aae4-6b1479663456.png',
 	},
 	// Hotel Booking, Backend
 	{
 		topic: 'feature',
-		link: 'https://github.com/Chrissiku/hotel-booking-backend',
+		link: 'https://github.com/elidakirigo/hotel-booking-backend',
 		title: 'Hotel Booking',
 		content: ``,
 		stacks: [
@@ -36,8 +36,8 @@ const allPosts = [
 				name: 'Bootstrap 5',
 			},
 		],
-		live: [true, 'https://github.com/Chrissiku/hotel-booking-backend'],
-		source: [true, 'https://github.com/Chrissiku/hotel-booking-backend'],
+		live: [true, 'https://github.com/elidakirigo/hotel-booking-backend'],
+		source: [true, 'https://github.com/elidakirigo/hotel-booking-backend'],
 		image: 'https://user-images.githubusercontent.com/101924220/209710207-edf76c66-5034-4cf0-aae4-6b1479663456.png',
 	},
 	// Folio
@@ -55,13 +55,13 @@ const allPosts = [
 			{ link: 'https://en.wikipedia.org/wiki/HTML', name: 'HTML' },
 		],
 		live: [true, 'https://chris-siku.vercel.app/'],
-		source: [true, 'https://github.com/Chrissiku/Chris-Siku-v1'],
+		source: [true, 'https://github.com/elidakirigo/Chris-Siku-v1'],
 		image: 'https://user-images.githubusercontent.com/101924220/209703582-eeb2e313-50a3-49a8-b133-8c5a6f5d49ea.png',
 	},
 	// House rental
 	{
 		topic: 'responsive',
-		link: 'https://chrissiku.github.io/real-estate/index.html',
+		link: 'https://elidakirigo.github.io/real-estate/index.html',
 		title: 'Real Estate',
 		content: ``,
 		stacks: [
@@ -73,8 +73,8 @@ const allPosts = [
 			{ link: 'https://en.wikipedia.org/wiki/CSS', name: 'CSS' },
 			{ link: 'https://www.javascript.com/', name: 'JavaScript' },
 		],
-		live: [true, 'https://chrissiku.github.io/real-estate/index.html'],
-		source: [true, 'https://github.com/Chrissiku/real-estate'],
+		live: [true, 'https://elidakirigo.github.io/real-estate/index.html'],
+		source: [true, 'https://github.com/elidakirigo/real-estate'],
 		image: 'https://user-images.githubusercontent.com/101924220/209696457-a0769944-e81f-49d9-a8a3-c3e5edcc2923.png',
 	},
 	// Progressive web app
@@ -90,13 +90,13 @@ const allPosts = [
 			{ link: 'https://web.dev/progressive-web-apps/', name: 'PWA' },
 		],
 		live: [true, 'https://pw-food-a.netlify.app/?'],
-		source: [true, 'https://github.com/Chrissiku/progressive-web-app'],
+		source: [true, 'https://github.com/elidakirigo/progressive-web-app'],
 		image: 'https://user-images.githubusercontent.com/101924220/209708303-486fef6d-7ddf-45b3-9518-87d8159406f5.png',
 	},
 	// Rails blog
 	{
 		topic: 'feature',
-		link: 'https://github.com/Chrissiku/Rails-Blog',
+		link: 'https://github.com/elidakirigo/Rails-Blog',
 		title: 'Rails Blog',
 		content: ``,
 		stacks: [
@@ -104,14 +104,14 @@ const allPosts = [
 			{ link: 'https://guides.rubyonrails.org/', name: 'Rails' },
 			{ link: 'https://en.wikipedia.org/wiki/HTML', name: 'HTML' },
 		],
-		live: [true, 'https://github.com/Chrissiku/Rails-Blog'],
-		source: [true, 'https://github.com/Chrissiku/Rails-Blog'],
+		live: [true, 'https://github.com/elidakirigo/Rails-Blog'],
+		source: [true, 'https://github.com/elidakirigo/Rails-Blog'],
 		image: 'https://user-images.githubusercontent.com/101924220/209710207-edf76c66-5034-4cf0-aae4-6b1479663456.png',
 	},
 	// Youtube clone
 	{
 		topic: 'web',
-		link: 'https://chrissiku.github.io/youtube_player_app/',
+		link: 'https://elidakirigo.github.io/youtube_player_app/',
 		title: 'Youtube clone',
 		content: ``,
 		stacks: [
@@ -119,8 +119,8 @@ const allPosts = [
 			{ link: 'https://en.wikipedia.org/wiki/CSS', name: 'CSS' },
 			{ link: 'https://www.javascript.com/', name: 'JavaScript' },
 		],
-		live: [true, 'https://chrissiku.github.io/youtube_player_app/'],
-		source: [true, 'https://github.com/Chrissiku/youtube_player_app'],
+		live: [true, 'https://elidakirigo.github.io/youtube_player_app/'],
+		source: [true, 'https://github.com/elidakirigo/youtube_player_app'],
 		image: 'https://user-images.githubusercontent.com/101924220/209709562-40608166-33c0-435e-ad68-beba52c1164f.png',
 	},
 	// Math magician
@@ -138,13 +138,13 @@ const allPosts = [
 			{ link: 'https://en.wikipedia.org/wiki/CSS', name: 'CSS' },
 		],
 		live: [true, 'https://chriscacl.netlify.app/'],
-		source: [true, 'https://github.com/Chrissiku/Math-magicians'],
+		source: [true, 'https://github.com/elidakirigo/Math-magicians'],
 		image: 'https://user-images.githubusercontent.com/101924220/209716069-062bb3a5-1ea6-4a8b-86a2-7892ac029808.png',
 	},
 	// Recipe App
 	{
 		topic: 'feature',
-		link: 'https://github.com/Chrissiku/Recipe-app',
+		link: 'https://github.com/elidakirigo/Recipe-app',
 		title: 'Recipe App',
 		content: ``,
 		stacks: [
@@ -156,8 +156,8 @@ const allPosts = [
 				name: 'Bootstrap 5',
 			},
 		],
-		live: [true, 'https://github.com/Chrissiku/Recipe-app'],
-		source: [true, 'https://github.com/Chrissiku/Recipe-app'],
+		live: [true, 'https://github.com/elidakirigo/Recipe-app'],
+		source: [true, 'https://github.com/elidakirigo/Recipe-app'],
 		image: 'https://user-images.githubusercontent.com/101924220/209710207-edf76c66-5034-4cf0-aae4-6b1479663456.png',
 	},
 	// Portfolio website
@@ -176,13 +176,13 @@ const allPosts = [
 			{ link: 'https://www.javascript.com/', name: 'JavaScript' },
 		],
 		live: [true, 'https://chris-siku.netlify.app/'],
-		source: [true, 'https://github.com/Chrissiku/my-portfolio'],
+		source: [true, 'https://github.com/elidakirigo/my-portfolio'],
 		image: 'https://user-images.githubusercontent.com/101924220/209657844-94b71a71-f467-45e5-87f0-1f2e65c9642f.png',
 	},
 	// Budget App
 	{
 		topic: 'feature',
-		link: 'https://github.com/Chrissiku/Budget-app',
+		link: 'https://github.com/elidakirigo/Budget-app',
 		title: 'Recipe App',
 		content: ``,
 		stacks: [
@@ -194,14 +194,14 @@ const allPosts = [
 				name: 'Bootstrap 5',
 			},
 		],
-		live: [true, 'https://github.com/Chrissiku/Budget-app'],
-		source: [true, 'https://github.com/Chrissiku/Budget-app'],
+		live: [true, 'https://github.com/elidakirigo/Budget-app'],
+		source: [true, 'https://github.com/elidakirigo/Budget-app'],
 		image: 'https://user-images.githubusercontent.com/101924220/209710207-edf76c66-5034-4cf0-aae4-6b1479663456.png',
 	},
 	// Napses Clone
 	{
 		topic: 'web',
-		link: 'https://chrissiku.github.io/napses_clone/',
+		link: 'https://elidakirigo.github.io/napses_clone/',
 		title: 'Napses Desktop clone',
 		content: ``,
 		stacks: [
@@ -209,14 +209,14 @@ const allPosts = [
 			{ link: 'https://en.wikipedia.org/wiki/CSS', name: 'CSS' },
 			{ link: 'https://www.javascript.com/', name: 'JavaScript' },
 		],
-		live: [true, 'https://chrissiku.github.io/napses_clone/'],
-		source: [true, 'https://github.com/Chrissiku/napses_clone'],
+		live: [true, 'https://elidakirigo.github.io/napses_clone/'],
+		source: [true, 'https://github.com/elidakirigo/napses_clone'],
 		image: 'https://user-images.githubusercontent.com/101924220/209715163-7875f85e-9b5c-4475-b71c-da59195c0bb2.png',
 	},
 	// .Leader-board
 	{
 		topic: 'feature',
-		link: 'https://chrissiku.github.io/Leaderboard/dist/',
+		link: 'https://elidakirigo.github.io/Leaderboard/dist/',
 		title: 'Leader-board',
 		content: ``,
 		stacks: [
@@ -224,14 +224,14 @@ const allPosts = [
 			{ link: 'https://en.wikipedia.org/wiki/CSS', name: 'CSS' },
 			{ link: 'https://www.javascript.com/', name: 'JavaScript' },
 		],
-		live: [true, 'https://chrissiku.github.io/Leaderboard/dist/'],
-		source: [true, 'https://github.com/Chrissiku/Leaderboard/'],
+		live: [true, 'https://elidakirigo.github.io/Leaderboard/dist/'],
+		source: [true, 'https://github.com/elidakirigo/Leaderboard/'],
 		image: 'https://user-images.githubusercontent.com/101924220/209706512-3513086d-bee8-4da8-8cdc-f312fd497357.png',
 	},
 	// Rest API TDD
 	{
 		topic: 'feature',
-		link: 'https://github.com/Chrissiku/tdd-rest-api',
+		link: 'https://github.com/elidakirigo/tdd-rest-api',
 		title: 'TDD Rest-API',
 		content: ``,
 		stacks: [
@@ -242,14 +242,14 @@ const allPosts = [
 				name: 'Bootstrap 5',
 			},
 		],
-		live: [true, 'https://github.com/Chrissiku/tdd-rest-api'],
-		source: [true, 'https://github.com/Chrissiku/tdd-rest-api'],
+		live: [true, 'https://github.com/elidakirigo/tdd-rest-api'],
+		source: [true, 'https://github.com/elidakirigo/tdd-rest-api'],
 		image: 'https://user-images.githubusercontent.com/101924220/209710207-edf76c66-5034-4cf0-aae4-6b1479663456.png',
 	},
 	// Personal website
 	{
 		topic: 'responsive',
-		link: 'https://github.com/Chrissiku/Chrissiku',
+		link: 'https://github.com/elidakirigo/elidakirigo',
 		title: 'Personal Website',
 		content: ``,
 		stacks: [
@@ -261,14 +261,14 @@ const allPosts = [
 			{ link: 'https://en.wikipedia.org/wiki/CSS', name: 'CSS' },
 			{ link: 'https://www.javascript.com/', name: 'JavaScript' },
 		],
-		live: [true, 'https://chrissiku.github.io/Chrissiku/'],
-		source: [true, 'https://github.com/Chrissiku/Chrissiku'],
+		live: [true, 'https://elidakirigo.github.io/elidakirigo/'],
+		source: [true, 'https://github.com/elidakirigo/elidakirigo'],
 		image: 'https://user-images.githubusercontent.com/101924220/209657843-5270123e-d89b-4480-98d4-84d6a7a1a074.png',
 	},
 	// Portfolio mobile
 	{
 		topic: 'responsive',
-		link: 'https://chrissiku.github.io/Portfolio-mobile/',
+		link: 'https://elidakirigo.github.io/Portfolio-mobile/',
 		title: 'Mobile Portfolio',
 		content: ``,
 		stacks: [
@@ -276,28 +276,28 @@ const allPosts = [
 			{ link: 'https://en.wikipedia.org/wiki/CSS', name: 'CSS' },
 			{ link: 'https://www.javascript.com/', name: 'JavaScript' },
 		],
-		live: [true, 'https://chrissiku.github.io/Portfolio-mobile/'],
-		source: [true, 'https://github.com/Chrissiku/Portfolio-mobile'],
+		live: [true, 'https://elidakirigo.github.io/Portfolio-mobile/'],
+		source: [true, 'https://github.com/elidakirigo/Portfolio-mobile'],
 		image: 'https://user-images.githubusercontent.com/101924220/209657847-0fece210-73f2-4e07-81ea-e5cbe391ccc9.png',
 	},
 	// House rental 2
 	{
 		topic: 'responsive',
-		link: 'https://chrissiku.github.io/house_rental/',
+		link: 'https://elidakirigo.github.io/house_rental/',
 		title: 'House Renatl',
 		content: ``,
 		stacks: [
 			{ link: 'https://www.learn-html.org/', name: 'HTML' },
 			{ link: 'https://en.wikipedia.org/wiki/CSS', name: 'CSS' },
 		],
-		live: [true, 'https://chrissiku.github.io/house_rental/'],
-		source: [true, 'https://github.com/Chrissiku/house_rental'],
+		live: [true, 'https://elidakirigo.github.io/house_rental/'],
+		source: [true, 'https://github.com/elidakirigo/house_rental'],
 		image: 'https://user-images.githubusercontent.com/101924220/209699847-d943b478-51d5-4413-b613-90a424c159a1.png',
 	},
 	// Catalog of things
 	{
 		topic: 'feature',
-		link: 'https://github.com/Chrissiku/Catalog-of-things',
+		link: 'https://github.com/elidakirigo/Catalog-of-things',
 		title: 'TDD Rest-API',
 		content: ``,
 		stacks: [
@@ -307,8 +307,8 @@ const allPosts = [
 				name: 'Bootstrap 5',
 			},
 		],
-		live: [true, 'https://github.com/Chrissiku/Catalog-of-things'],
-		source: [true, 'https://github.com/Chrissiku/Catalog-of-things'],
+		live: [true, 'https://github.com/elidakirigo/Catalog-of-things'],
+		source: [true, 'https://github.com/elidakirigo/Catalog-of-things'],
 		image: 'https://user-images.githubusercontent.com/101924220/209710207-edf76c66-5034-4cf0-aae4-6b1479663456.png',
 	},
 	// SpaceX
@@ -330,13 +330,13 @@ const allPosts = [
 			},
 		],
 		live: [true, 'https://space-x-traveler.netlify.app/'],
-		source: [true, 'https://github.com/Chrissiku/Bookstore-CMS'],
+		source: [true, 'https://github.com/elidakirigo/Bookstore-CMS'],
 		image: 'https://user-images.githubusercontent.com/101924220/209657850-efc6e2d6-249c-4e1e-84ad-d8adebabe9f2.png',
 	},
 	// .Todo list app
 	{
 		topic: 'feature',
-		link: 'https://chrissiku.github.io/To-Do-List/dist',
+		link: 'https://elidakirigo.github.io/To-Do-List/dist',
 		title: 'Todo-list App',
 		content: ``,
 		stacks: [
@@ -344,14 +344,14 @@ const allPosts = [
 			{ link: 'https://en.wikipedia.org/wiki/CSS', name: 'CSS' },
 			{ link: 'https://www.javascript.com/', name: 'JavaScript' },
 		],
-		live: [true, 'https://chrissiku.github.io/To-Do-List/dist'],
-		source: [true, 'https://github.com/Chrissiku/To-Do-List'],
+		live: [true, 'https://elidakirigo.github.io/To-Do-List/dist'],
+		source: [true, 'https://github.com/elidakirigo/To-Do-List'],
 		image: 'https://user-images.githubusercontent.com/101924220/209657853-1b59a194-c885-49ce-9729-5dcd47010347.png',
 	},
 	// web conferencing
 	{
 		topic: 'web',
-		link: 'https://chrissiku.github.io/Conferencing-webiste/',
+		link: 'https://elidakirigo.github.io/Conferencing-webiste/',
 		title: 'Web Conferencing',
 		content: ``,
 		stacks: [
@@ -359,8 +359,8 @@ const allPosts = [
 			{ link: 'https://en.wikipedia.org/wiki/CSS', name: 'CSS' },
 			{ link: 'https://www.javascript.com/', name: 'JavaScript' },
 		],
-		live: [true, 'https://chrissiku.github.io/Conferencing-webiste/'],
-		source: [true, 'https://github.com/Chrissiku/capstone_project_one'],
+		live: [true, 'https://elidakirigo.github.io/Conferencing-webiste/'],
+		source: [true, 'https://github.com/elidakirigo/capstone_project_one'],
 		image: 'https://user-images.githubusercontent.com/101924220/209657842-eb0095ed-e8af-48f8-ac47-277991121ec7.png',
 	},
 	// Book store cms
@@ -379,13 +379,13 @@ const allPosts = [
 			{ link: 'https://www.javascript.com/', name: 'JavaScript' },
 		],
 		live: [true, 'https://storebookcms.netlify.app/'],
-		source: [true, 'https://github.com/Chrissiku/Bookstore-CMS'],
+		source: [true, 'https://github.com/elidakirigo/Bookstore-CMS'],
 		image: 'https://user-images.githubusercontent.com/101924220/209657836-0965c8de-8869-4308-b779-405bfa55cdc1.png',
 	},
 	// Breakout Game
 	{
 		topic: 'feature',
-		link: 'https://chrissiku.github.io/100-days-of-javascript/breakout_game/dist/',
+		link: 'https://elidakirigo.github.io/100-days-of-javascript/breakout_game/dist/',
 		title: 'Breakout Game',
 		content: ``,
 		stacks: [
@@ -394,8 +394,8 @@ const allPosts = [
 			{ link: 'https://www.javascript.com/', name: 'JavaScript' },
 			{ link: 'https://webpack.js.org/', name: 'Webpack' },
 		],
-		live: [true, 'https://chrissiku.github.io/100-days-of-javascript/breakout_game/dist/'],
-		source: [true, 'https://github.com/Chrissiku/100-days-of-javascript/tree/main/breakout_game'],
+		live: [true, 'https://elidakirigo.github.io/100-days-of-javascript/breakout_game/dist/'],
+		source: [true, 'https://github.com/elidakirigo/100-days-of-javascript/tree/main/breakout_game'],
 		image: 'https://user-images.githubusercontent.com/101924220/209657840-44b0c28a-ddba-41d2-ace1-ac6b9be60c06.png',
 	},
 ]
@@ -407,17 +407,9 @@ allPosts.map((element) => {
               <div class="item-card">
                 <h4><a target="_blank" class="title-link" href="${element.link}">${element.title}</a></h4>
                 <p>${element.content}</p>
-                <ul class="tag-link">
-                    ` +
-		element.stacks
-			.map((stack) => {
-				return `<li><a target="_blank" href="${stack.link}">${stack.name}</a></li>`
-			})
-			.join('') +
+               ` +
 		`
-                </ul>
-                <div class="item-image">              
-                  
+		<div class="item-image"> 
                   <a target="_blank" class="link-image" href="${element.live[1]}">
                       <i class="fa fa-eye"></i>
                   </a>
@@ -425,10 +417,19 @@ allPosts.map((element) => {
                   <a target="_blank" class="link-view" href="${element.source[1]}">
                       <i class="fab fa-github"></i>
                   </a>
-                  <img src="${element.image}" alt="image ${element.title}" />
-                </div>
-              </div>
-            </div>
+                  <img src="${element.image}" alt="image ${element.title}" /> 
+                </div>` +
+		`  <ul class="tag-link">` +
+		element.stacks
+			.map((stack) => {
+				return `<li><a target="_blank" href="${stack.link}">${stack.name}</a></li>`
+			})
+			.join('') +
+		`
+		</ul>` +
+		`
+              </div> 
+			  </div>
       `
 
 	portfolio.innerHTML += html
---
+++ portfolio-mini/README.md
@@ -0,0 +1,17 @@
+# 💼 Responsive Mini Portfolio
+## [curtesy of ](https://youtu.be/mq0xJxOTiYo)
+### 💼 Responsive Mini Portfolio
+
+- Responsive mini portfolio website Using HTML CSS & JavaScript
+- Contains animations css.
+- Includes a light and dark mode.
+- Developed first with the Mobile First methodology, then for desktop.
+- Compatible with all mobile devices and with a beautiful and pleasant user interface.
+
+
+
+## TECHNOLOGIES
+>  HTML CSS & JavaScript
+
+## LINKS
+### [use of remix icon](https://remixicon.com/)
