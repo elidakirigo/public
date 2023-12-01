d914940026460c56c8425190dbeec7bfe15b9d40
elida <elidakirigo@gmail.com>
2023-12-01 21:03:06 +0300

    `

--- react-portfolio/tailwind.config.js
+++ react-portfolio/tailwind.config.js
@@ -1,9 +1,8 @@
 /** @type {import('tailwindcss').Config} */
-export default {
-  content: [],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+module.exports = {
+	content: ['./src/**/*.{js,jsx,ts,tsx}'],
+	theme: {
+		extend: {},
+	},
+	plugins: [],
 }
-
