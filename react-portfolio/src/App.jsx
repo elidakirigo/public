171d5755304f8a2cbb3766da5b0a632197c6ab04
elida <elidakirigo@gmail.com>
2023-12-01 21:11:14 +0300

    `

--- react-portfolio/src/App.jsx
+++ react-portfolio/src/App.jsx
@@ -3,11 +3,14 @@ import { Experience } from "./components/Experience";
 
 function App() {
   return (
-    <Canvas shadows camera={{ position: [3, 3, 3], fov: 30 }}>
-      <color attach="background" args={["#ececec"]} />
-      <Experience />
-    </Canvas>
-  );
+		<>
+			<h1 className='text-3xl font-bold underline'>Hello world!</h1>
+			<Canvas shadows camera={{ position: [3, 3, 3], fov: 30 }}>
+				<color attach='background' args={['#ececec']} />
+				<Experience />
+			</Canvas>
+		</>
+  )  
 }
 
 export default App;
