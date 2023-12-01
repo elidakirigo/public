ec7a2859d5ffe5559e7a310b2b680ec7da1bdd9d
elida <elidakirigo@gmail.com>
2023-12-01 21:52:16 +0300

    `

--- react-portfolio/src/App.jsx
+++ react-portfolio/src/App.jsx
@@ -1,12 +1,15 @@
 import { Canvas } from '@react-three/fiber'
 import { Experience } from './components/Experience'
+import { ScrollControls } from '@react-three/drei'
 
 function App() {
 	return (
 		<>
 			<Canvas shadows camera={{ position: [3, 3, 3], fov: 30 }}>
 				<color attach='background' args={['#ececec']} />
-				<Experience />
+				<ScrollControls pages={3} damping={0.1}>
+					<Experience />
+				</ScrollControls>
 			</Canvas>
 		</>
 	)
