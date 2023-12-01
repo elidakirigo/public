2dddf3f3c974c5e35a16019ea4d7c26b616d068b
elida <elidakirigo@gmail.com>
2023-12-01 21:14:39 +0300

    `

--- react-portfolio/src/App.jsx
+++ react-portfolio/src/App.jsx
@@ -1,16 +1,16 @@
-import { Canvas } from "@react-three/fiber";
-import { Experience } from "./components/Experience";
+import { Canvas } from '@react-three/fiber'
+import { Experience } from './components/Experience'
 
 function App() {
-  return (
+	return (
 		<>
-			<h1 className='text-3xl font-bold underline'>Hello world!</h1>
 			<Canvas shadows camera={{ position: [3, 3, 3], fov: 30 }}>
 				<color attach='background' args={['#ececec']} />
 				<Experience />
 			</Canvas>
+			<h1 className='text-3xl font-bold underline text-red-600'>Hello world!</h1>
 		</>
-  )  
+	)
 }
 
-export default App;
+export default App
