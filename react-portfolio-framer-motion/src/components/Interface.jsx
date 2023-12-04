07ea6a053a3148d17a68029f8b6a28dd084beb2b
elida <elidakirigo@gmail.com>
2023-12-04 14:06:35 +0300

    `

--- react-portfolio-framer-motion/src/components/Interface.jsx
+++ react-portfolio-framer-motion/src/components/Interface.jsx
@@ -1,23 +1,38 @@
 const Section = (props) => {
 	const { children } = props
 
-	return <section>{children}</section>
+	return <section className={`h-screen w-screen p-8 max-w-screen-2xl mx-auto flex flex-col items-start justify-center`}>{children}</section>
 }
 export const Interface = () => {
 	return (
-		<>
-			<Section>
-				<h1>About</h1>
-			</Section>
-			<Section>
-				<h1>Skills</h1>
-			</Section>
+		<div className='flex flex-col items-center w-screen'>
+			<AboutSection/>
+			<SkillsSection/>
 			<Section>
 				<h1>Projects</h1>
 			</Section>
 			<Section>
 				<h1>Contacts</h1>
 			</Section>
-		</>
+		</div>
 	)
+}
+
+const AboutSection = () => {
+	return (
+		<Section>
+			<h1 className='text-6xl font-extrabold leading-snug'>
+				Hi, I'm <br />
+				<span className='bg-white px-1 italic'>Elida wanjiku</span>
+			</h1>
+			<p className='text-lg text-gray-600 mt-4'>I Make Youtube videos to help developers</p>
+		</Section>
+	)
+}
+
+const skills = [
+	{}
+]
+const SkillsSection = ()=>{
+
 }
