e5c382f9a5e7e3c017f790969a2899c9e21182d9
elida <elidakirigo@gmail.com>
2023-12-06 21:08:35 +0300

    `

--- responsive-portfolio-website-Alexa/assets/js/main.js
+++ responsive-portfolio-website-Alexa/assets/js/main.js
@@ -4,31 +4,31 @@ const navMenu = document.getElementById('nav-menu'),
 	navToggle = document.getElementById('nav-toggle'),
 	navClose = document.getElementById('nav-close')
 
-// /*===== MENU SHOW =====*/
-// /* Validate if constant exists */
-// if (navToggle) {
-// 	navToggle.addEventListener('click', () => {
-// 		navMenu.classList.add('show-menu')
-// 	})
-// }
+/*===== MENU SHOW =====*/
+/* Validate if constant exists */
+if (navToggle) {
+	navToggle.addEventListener('click', () => {
+		navMenu.classList.add('show-menu')
+	})
+}
 
-// /*===== MENU HIDDEN =====*/
-// /* Validate if constant exists */
-// if (navClose) {
-// 	navClose.addEventListener('click', () => {
-// 		navMenu.classList.remove('show-menu')
-// 	})
-// }
+/*===== MENU HIDDEN =====*/
+/* Validate if constant exists */
+if (navClose) {
+	navClose.addEventListener('click', () => {
+		navMenu.classList.remove('show-menu')
+	})
+}
 
-// /*==================== REMOVE MENU MOBILE ====================*/
-// const navLink = document.querySelectorAll('.nav__link')
+/*==================== REMOVE MENU MOBILE ====================*/
+const navLink = document.querySelectorAll('.nav__link')
 
-// function linkAction() {
-// 	const navMenu = document.getElementById('nav-menu')
-// 	// When we click on each nav__link, we remove the show-menu class
-// 	navMenu.classList.remove('show-menu')
-// }
-// navLink.forEach((n) => n.addEventListener('click', linkAction))
+function linkAction() {
+	const navMenu = document.getElementById('nav-menu')
+	// When we click on each nav__link, we remove the show-menu class
+	navMenu.classList.remove('show-menu')
+}
+navLink.forEach((n) => n.addEventListener('click', linkAction))
 
 // /*==================== ACCORDION SKILLS ====================*/
 // const skillsContent = document.getElementsByClassName('skills__content'),
