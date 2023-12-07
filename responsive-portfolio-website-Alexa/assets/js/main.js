72ba6ff8e8dbd353d4e97a6eca33079660948a0c
elida <elidakirigo@gmail.com>
2023-12-07 19:52:47 +0300

    `

--- responsive-portfolio-website-Alexa/assets/js/main.js
+++ responsive-portfolio-website-Alexa/assets/js/main.js
@@ -30,44 +30,43 @@ function linkAction() {
 }
 navLink.forEach((n) => n.addEventListener('click', linkAction))
 
-// /*==================== ACCORDION SKILLS ====================*/
-// const skillsContent = document.getElementsByClassName('skills__content'),
-// 	skillsHeader = document.querySelectorAll('.skills__header')
+/*==================== ACCORDION SKILLS ====================*/
+const skillsContent = document.getElementsByClassName('skills__content'),
+	skillsHeader = document.querySelectorAll('.skills__header')
 
-// function toggleSkills() {
-// 	let itemClass = this.parentNode.getElementsByClassName
+function toggleSkills() {
+	let itemClass = this.parentNode.className
 
-// 	for (let i = 0; i < skillsContent.length; i++) {
-// 		skillsContent[i].className = 'skills__content skills__close'
-// 	}
-// 	console.log(this.parentNode, itemClass === 'skills__content skills__close')
-// 	if (itemClass === 'skills__content skills__close') {
-// 		this.parentNode.className = 'skills__content skills__open'
-// 	}
-// }
+	for (let i = 0; i < skillsContent.length; i++) {
+		skillsContent[i].className = 'skills__content skills__close'
+	}
+	if (itemClass === 'skills__content skills__close') {
+		this.parentNode.className = 'skills__content skills__open'
+	}
+}
 
-// skillsHeader.forEach((el) => {
-// 	el.addEventListener('click', toggleSkills)
-// })
-// /*==================== QUALIFICATION TABS ====================*/
-// const tabs = document.querySelectorAll('[data-target'),
-// 	tabContents = document.querySelectorAll('[data-content')
+skillsHeader.forEach((el) => {
+	el.addEventListener('click', toggleSkills)
+})
+/*==================== QUALIFICATION TABS ====================*/
+const tabs = document.querySelectorAll('[data-target'),
+	tabContents = document.querySelectorAll('[data-content')
 
-// tabs.forEach((tab) => {
-// 	tab.addEventListener('click', () => {
-// 		const target = document.querySelector(tab.dataset.target)
+tabs.forEach((tab) => {
+	tab.addEventListener('click', () => {
+		const target = document.querySelector(tab.dataset.target)
 
-// 		tabContents.forEach((tabContent) => {
-// 			tabContent.classList.remove('qualification__active')
-// 		})
-// 		target.classList.add('qualification__active')
+		tabContents.forEach((tabContent) => {
+			tabContent.classList.remove('qualification__active')
+		})
+		target.classList.add('qualification__active')
 
-// 		tabs.forEach((tab) => {
-// 			tab.classList.remove('qualification__active')
-// 		})
-// 		tab.classList.add('qualification__active')
-// 	})
-// })
+		tabs.forEach((tab) => {
+			tab.classList.remove('qualification__active')
+		})
+		tab.classList.add('qualification__active')
+	})
+})
 // /*==================== SERVICES MODAL ====================*/
 // const modalViews = document.querySelectorAll('.services__modal'),
 // 	modalBtns = document.querySelectorAll('.services__button'),
