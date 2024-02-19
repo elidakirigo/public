3ffb7d438d6718a48be38af28c2b9e0ae6eb4fa7
elida <elidakirigo@gmail.com>
2024-02-19 13:00:00 +0300

    -

--- portfolio-react-framer-motion/src/components/ScrollManager.jsx
+++ portfolio-react-framer-motion/src/components/ScrollManager.jsx
@@ -16,6 +16,12 @@ const ScrollManager = (props) => {
 		}
 
 		const curSection = Math.floor(data.scroll.current * data.pages)
+		if (data.scroll.current > lastScroll.current && curSection === 0) {
+			onSectionChange(1)
+		}
+		if (data.scroll.current < lastScroll.current && data.scroll.current < 1 / (data.pages - 1)) {
+			onSectionChange(0)
+		}
 	})
 	return null
 }
