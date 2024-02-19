21a2a2933f716d7c99d310f74270fc21a64699aa
elida <elidakirigo@gmail.com>
2024-02-19 13:00:00 +0300

    -

--- portfolio-react-framer-motion/src/components/ScrollManager.jsx
+++ portfolio-react-framer-motion/src/components/ScrollManager.jsx
@@ -1,5 +1,22 @@
+import { useScroll } from '@react-three/drei'
+import { useFrame } from '@react-three/fiber'
+import { useRef } from 'react'
+
 const ScrollManager = (props) => {
 	const { section, onSectionChange } = props
+
+	const data = useScroll()
+	const lastScroll = useRef(0)
+	const isAnimating = useRef(false)
+
+	useFrame(() => {
+		if (isAnimating.current) {
+			lastScroll.current = data.scroll.current
+			return
+		}
+
+		const curSection = Math.floor(data.scroll.current * data.pages)
+	})
 	return null
 }
 
