49e8cc3a0ad456470bcdcf28efe6741c3ded7360
elida <elidakirigo@gmail.com>
2024-03-04 13:00:00 +0300

    -

--- portfolio-react-threeJS/client/src/App.jsx
+++ portfolio-react-threeJS/client/src/App.jsx
@@ -1,16 +1,18 @@
 import './App.css'
 import { Route, BrowserRouter as Router, Routes } from 'react-router-dom'
+import Navbar from './components/Navbar'
+import { About, Contact, Home, Projects } from './pages'
 
 function App() {
 	return (
 		<main className=' bg-slate-300/20'>
 			<Router>
-				Navbar
+				<Navbar/>
 				<Routes>
-					<Route path='/' element={'Home'} />
-					<Route path='/about' element={'About'} />
-					<Route path='/project' element={'Project'} />
-					<Route path='/contact' element={'Contact'} />
+					<Route path='/' element={<Home/>} />
+					<Route path='/about' element={<About/>} />
+					<Route path='/project' element={<Projects/>} />
+					<Route path='/contact' element={<Contact/>} />
 				</Routes>
 			</Router>
 		</main>
---
+++ portfolio-react-threeJS/client/src/pages/index.js
@@ -0,0 +1,6 @@
+import Home from './Home'
+import Contact from './Contact'
+import About from './About'
+import Projects from './Projects'
+
+export { Home, Contact, About, Projects }
