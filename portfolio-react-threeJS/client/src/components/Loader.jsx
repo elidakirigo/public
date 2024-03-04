541524855e2a8649435a82e60abfac187d4feaae
elida <elidakirigo@gmail.com>
2024-03-04 13:00:00 +0300

    -

---
+++ portfolio-react-threeJS/client/src/components/Loader.jsx
@@ -0,0 +1,9 @@
+const Loader = () => {
+	return (
+		<div className='flex justify-center items-center'>
+			<div className='w-20 h-20 border-2 border-opacity-20 border-blue-500 border-t-blue-500 rounded-full animate-spin' />
+		</div>
+	)
+}
+
+export default Loader
--- portfolio-react-threeJS/client/src/pages/Home.jsx
+++ portfolio-react-threeJS/client/src/pages/Home.jsx
@@ -1,10 +1,15 @@
 import { Canvas } from '@react-three/fiber'
+// NEW
+import { Suspense } from 'react'
+import Loader from '../components/Loader'
 
 const Home = () => {
 	return (
 		<section className='w-full h-screen relative'>
 			{/* <div className='absolute top-28 left-0 right-0 z-10 flex items-center justify-center'>popup</div> */}
-			<Canvas className='w-full h-full bg-transparent'></Canvas>
+			<Canvas className='w-full h-full bg-transparent' camera={{ near: 0.1, far: 1000 }}>
+				<Suspense fallback={<Loader />}></Suspense>
+			</Canvas>
 		</section>
 	)
 }
