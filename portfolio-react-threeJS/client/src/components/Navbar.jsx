7c7aae1550f65626f458a55385c0b816e04bf339
elida <elidakirigo@gmail.com>
2024-03-04 13:00:00 +0300

    -

--- portfolio-react-threeJS/client/src/components/Navbar.jsx
+++ portfolio-react-threeJS/client/src/components/Navbar.jsx
@@ -4,7 +4,7 @@ const Navbar = () => {
 	return (
 		<header className='header'>
 			<NavLink to='/' className='w-10 h-10 rounded-lg bg-white items-center justify-center flex font-bold shadow-md'>
-				<p className='blue-gradient_text'>AH</p>
+				<p className='blue-gradient_text'>EK</p>
 			</NavLink>
 			<nav className='flex text-lg gap-7 font-medium'>
 				{/* NEW */}
--- portfolio-react-threeJS/client/src/pages/Contact.jsx
+++ portfolio-react-threeJS/client/src/pages/Contact.jsx
@@ -1,5 +1,11 @@
 const Contact = () => {
-	return <div></div>
+	return (
+		<section className='relative flex lg:flex-row flex-col max-container'>
+			<div className='flex-1 min-w-[50%]'>
+				<h1>Get in touch</h1>
+			</div>
+		</section>
+	)
 }
 
 export default Contact
