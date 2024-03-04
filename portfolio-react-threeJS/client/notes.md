c0828a1f89cfdf75942a5e07be400c7054f9dc8e
elida <elidakirigo@gmail.com>
2024-03-04 13:00:00 +0300

    -

--- portfolio-react-threeJS/client/notes.md
+++ portfolio-react-threeJS/client/notes.md
@@ -11,7 +11,7 @@ tailwind with vite - https://tailwindcss.com/docs/guides/vite
 [assets folder](https://drive.google.com/file/d/1TACDN8qFpi_NW66I4p48R9_MUmx-JT3P/edit?pli=1)
 [github gist](https://gist.github.com/adrianhajdin/7bc0603ee0b1bd2872b80a62c24b5e5f#file-index-js)
 
-npm install react-router-dom @react-three/fiber @react-three/drei @react-spring/three @emailjs/browser
+npm install react-router-dom @react-three/fiber @react-three/drei @react-spring/three @emailjs/browser react-vertical-timeline-component
 
 3d models - https://sketchfab.com/
 
@@ -23,3 +23,5 @@ emailJS admin - https://dashboard.emailjs.com/admin
 	- password - ELIDAWANJIKU12345
 
 gltf loader / viewer - https://gltf.pmnd.rs/
+
+[react vertical timeline](https://www.npmjs.com/package/react-vertical-timeline-component)
--- portfolio-react-threeJS/client/src/pages/Contact.jsx
+++ portfolio-react-threeJS/client/src/pages/Contact.jsx
@@ -4,12 +4,16 @@ import { Canvas } from '@react-three/fiber'
 
 import Fox from '../models/Fox'
 import Loader from '../components/Loader'
+import UseAlert from '../hooks/UseAlert'
+import Alert from '../components/Alert'
 
 const Contact = () => {
 	const formRef = useRef(null)
 	const [form, setForm] = useState({ name: '', email: '', message: '' })
 	const [isLoading, setIsLoading] = useState(false)
 	const [currentAnimation, setCurrentAnimation] = useState('idle')
+
+	const { alert, showAlert, hideAlert } = UseAlert()
 
 	const handlechange = (e) => setForm({ ...form, [e.target.name]: e.target.value })
 
@@ -37,9 +41,11 @@ const Contact = () => {
 			.then(() => {
 				setIsLoading(false)
 				// TODO: show success message
+				showAlert({ show: true, text: 'Message sent successfully', type: 'success' })
 				// TODO: Hide an alert
 
 				setTimeout(() => {
+					hideAlert()
 					setCurrentAnimation('idle')
 					setForm({ name: '', email: '', message: '' })
 				}, [3000])
@@ -49,6 +55,7 @@ const Contact = () => {
 			.catch((error) => {
 				setIsLoading(false)
 				setCurrentAnimation('idle')
+				showAlert({ show: true, text: 'I didnt recieve your message', type: 'danger' })
 				console.log(error)
 				// TODO: show error message
 			})
@@ -56,6 +63,7 @@ const Contact = () => {
 
 	return (
 		<section className='relative flex lg:flex-row flex-col max-container'>
+			{alert.show && <Alert {...alert} />}
 			<div className='flex-1 min-w-[50%] flex flex-col'>
 				<h1 className='head-text'>Get in touch</h1>
 
