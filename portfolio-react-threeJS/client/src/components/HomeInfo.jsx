3eb349cf6f50c8df252cb360179000c9a401281d
elida <elidakirigo@gmail.com>
2024-03-04 13:00:00 +0300

    -

--- portfolio-react-threeJS/client/src/components/HomeInfo.jsx
+++ portfolio-react-threeJS/client/src/components/HomeInfo.jsx
@@ -1,10 +1,24 @@
+import { Link } from 'react-router-dom'
 
-const HomeInfo = () => {
-  return (
-	<div>
-	  popup
+const InfoBox = ({ text, link, btnText }) => (
+	<div className='info-box'>
+		<p className='font-medium sm:text-xl text-center'>{text} </p> <Link to={link}>{btnText}</Link>
 	</div>
-  )
+)
+
+const renderContent = {
+	1: (
+		<h1 className='sm-text-xl sm:leading-snug text-center neo-brutalism-blue py-4 px-8 text-white mx-5'>
+			hi i am <span className='font-semibold'>Elida</span> 🖐 <br /> A software Engineer from kenya🏁
+		</h1>
+	),
+	2: <InfoBox text='worked with many companies and picked up many skills along the way' link={'/about'} btnText='Learn more' />,
+	3: <h1 className='sm-text-xl sm:leading-snug text-center neo-brutalism-blue py-4 px-8 text-white mx-5'>3</h1>,
+	4: <h1 className='sm-text-xl sm:leading-snug text-center neo-brutalism-blue py-4 px-8 text-white mx-5'>4</h1>,
+}
+// NEW
+const HomeInfo = ({ currentStage }) => {
+	return renderContent[currentStage] || null
 }
 
 export default HomeInfo
--- portfolio-react-threeJS/client/src/pages/Home.jsx
+++ portfolio-react-threeJS/client/src/pages/Home.jsx
@@ -45,7 +45,7 @@ const Home = () => {
 	const [planeScale, planePosition] = adjustPlaneForScreenSize()
 	return (
 		<section className='w-full h-screen relative'>
-			<div className='absolute top-28 left-0 right-0 z-10 flex items-center justify-center'>{currentStage&&<HomeInfo currentStage={currentStage} />}</div>
+			<div className='absolute top-28 left-0 right-0 z-10 flex items-center justify-center'>{currentStage && <HomeInfo currentStage={currentStage} />}</div>
 			<Canvas className={`w-full h-full bg-transparent ${isRotating ? 'cursor-grabbing' : 'cursor-grab'}`} camera={{ near: 0.1, far: 1000 }}>
 				<Suspense fallback={<Loader />}>
 					<directionalLight position={[1, 1, 1]} intensity={2} />
